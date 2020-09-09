747ea8f0af189dda41366d8296e007998fe301a2
Farhan Rabbaanii <farhanrbnn@gmail.com>
2020-09-09 19:12:11 +0700

    fixing mainPage content

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -1,6 +1,6 @@
 <template>
 	<div id="mainPage">
-		<h3 class="mx-auto">Farhan Rabbaanii</h3>
+		<h3 align="center">Farhan Rabbaanii</h3>
 		<img v-bind:src="require('@/assets/computer.jpg')" width="1900" height="900">
 	</div>
 </template>
@@ -14,6 +14,9 @@
 <style scoped>
 	h3 {
 		position: absolute;
+		margin-top: 100px;
+		width: 1900px;
+		font-size: 60px;
 
 	}
 	#mainPage {
